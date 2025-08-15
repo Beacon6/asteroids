4cc85983746b72e606a924e22956064879fd8805
Beacon6 <61936260+Beacon6@users.noreply.github.com>
2025-08-15 22:49:08 +0200

    feat: change hp text color based on value

--- app/ui/score_panel.py
+++ app/ui/score_panel.py
@@ -11,6 +11,10 @@ class ScorePanel:
         self.font_file = font_file
         self.font_size = font_size
         self.font = self._setup_font()
+        self.h_offset = 16
+        self.v_offset = 16
+        self.fg_color = 'green'
+        self.bg_color = 'black'
 
     def _setup_font(self) -> Font:
         font = freetype.Font(self.font_file, self.font_size)
@@ -18,7 +22,22 @@ class ScorePanel:
         return font
 
     def render(self, data_source: Player) -> None:
+        assert data_source.hp in range(1, 4)
+        match data_source.hp:
+            case 3:
+                hp_color = self.fg_color
+            case 2:
+                hp_color = 'orange'
+            case 1:
+                hp_color = 'red'
+
         hp_text = f'Health: {data_source.hp}'
         score_text = f'Score: {data_source.score}'
-        self.font.render_to(self.render_target, (8, 8), hp_text, 'green', 'black')
-        self.font.render_to(self.render_target, (8, 32), score_text, 'green', 'black')
+        self.font.render_to(self.render_target, (self.h_offset, self.v_offset), hp_text, hp_color, self.bg_color)
+        self.font.render_to(
+            self.render_target,
+            (self.h_offset, 8 + self.v_offset + self.font_size),
+            score_text,
+            self.fg_color,
+            self.bg_color,
+        )
